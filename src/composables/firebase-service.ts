24c8ef9b38db40aa046540c94a98584266ae735d
Tyler Clark <tyler@tylerclark.com>
2023-10-30 15:41:53 -0400

    Composable basics

---
+++ src/composables/firebase-service.ts
@@ -0,0 +1,183 @@
+import { toRefs, reactive, ref } from "vue";
+
+import { Capacitor } from "@capacitor/core";
+import { getApp, getApps, initializeApp } from "firebase/app";
+
+import {
+  getFirestore,
+  addDoc,
+  doc,
+  deleteDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  query,
+  collection,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+  onSnapshot,
+} from "firebase/firestore";
+import { getFunctions, httpsCallable, connectFunctionsEmulator } from "firebase/functions";
+import {
+  getAuth,
+  OAuthProvider,
+  onAuthStateChanged,
+  signInWithCredential,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  fetchSignInMethodsForEmail,
+  sendPasswordResetEmail,
+  signOut,
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+  indexedDBLocalPersistence,
+  initializeAuth,
+  updateProfile,
+} from "firebase/auth";
+// import { getStorage, ref as firebaseRef, getDownloadURL } from 'firebase/storage';
+
+// import { Network } from '@capacitor/network';
+// import { Pet } from './types/pets';
+// import * as Sentry from '@sentry/vue';
+
+import { FirebaseAuthentication } from "@capacitor-firebase/authentication";
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBViTc7v2QNNR4gsGtkH9nR9l7e21FvBBc",
+  authDomain: "app.umbrella.vet",
+  projectId: "umbrellavettest",
+  storageBucket: "umbrellavettest_chat_media",
+  messagingSenderId: "95746765505",
+  appId: "1:95746765505:web:07e06915c6c31494cf1a40",
+};
+
+const app = getApps().length === 0 ? initializeApp(FIREBASE_CONFIG) : getApps()[0];
+const db = getFirestore(app);
+const functions = getFunctions(app);
+
+export const getFirebaseAuth = () => {
+  if (Capacitor.isNativePlatform()) {
+    return initializeAuth(getApp(), {
+      persistence: indexedDBLocalPersistence,
+    });
+  } else {
+    return getAuth(app);
+  }
+};
+
+const auth = getFirebaseAuth();
+
+if (Capacitor.isNativePlatform()) {
+  initializeAuth(app, {
+    persistence: indexedDBLocalPersistence,
+  });
+}
+// const storage = getStorage(app);
+
+// const emulatorLocation = 'MY_IP';
+// if (process.env.NODE_ENV === 'development' && Capacitor.isNativePlatform()) {
+//   console.log(`** Will call functions from ${emulatorLocation}:5004 ***`);
+//   connectFunctionsEmulator(functions, emulatorLocation, 5004);
+// }
+
+type TState = {
+  error: Error | null;
+  loading: boolean;
+  user: any; //firebase.auth.UserCredential | null | firebase.User;
+  userData: any;
+  initialized: boolean;
+  isNative: boolean;
+  subscribed: boolean;
+};
+
+export const state = reactive<TState>({
+  user: null,
+  loading: true,
+  error: null,
+  userData: null,
+  initialized: false,
+  isNative: false,
+  subscribed: false,
+});
+
+export const googleSignin = async (isNative: Boolean) => {
+  console.log("googleSignin");
+  console.log("window.location.href", window.location.href);
+
+  // 1. Create credentials on the native layer
+  const result = await FirebaseAuthentication.signInWithGoogle({
+    scopes: ["profile", "email"],
+  });
+  console.log("google response", result);
+  // 2. Sign in on the web layer using the id token
+  const credential = GoogleAuthProvider.credential(result.credential?.idToken);
+  await signInWithCredential(auth, credential);
+  return result.user;
+};
+
+// function getAppleDisplayName(providerData: any) {
+//   if (providerData) {
+//     for (let i = 0; i < providerData.length; i++) {
+//       if (providerData[i].providerId === 'apple.com') {
+//         return providerData[i].displayName;
+//       }
+//     }
+//   }
+//   return 'Apple User';
+// }
+
+// async function updateDetails(name: string, email: string | null | undefined) {
+//   const user = auth.currentUser;
+//   if (user) {
+//     const updateUser = httpsCallable(functions, 'updateUserCall');
+//     await updateUser({ name, email });
+//     state.user.email = email;
+//     state.user.displayName = name;
+//     state.user.emailVerified = true;
+//   }
+// }
+
+export const appleSignin = async () => {
+  console.log("appleSignin");
+  console.log("environment", process.env.NODE_ENV);
+  const result = await FirebaseAuthentication.signInWithApple({ skipNativeAuth: false, scopes: ["name", "email"] });
+  console.log("appleSignin:FirebaseAuthentication.signInWithApple()", JSON.stringify(result, null, 2));
+  const provider = new OAuthProvider("apple.com");
+  console.log('appleSignin:OAuthProvider("apple.com")', JSON.stringify(provider, null, 2));
+  const credential = provider.credential({
+    idToken: result.credential?.idToken,
+    rawNonce: result.credential?.nonce,
+  });
+  console.log("appleSignin:provider.credential", JSON.stringify(credential, null, 2));
+  const signinWith = await signInWithCredential(auth, credential);
+  console.log("appleSignin:signinWithCredential", JSON.stringify(signinWith, null, 2));
+  // const appleName = getAppleDisplayName(result?.user?.providerData);
+  // await updateDetails(appleName, result.user?.email);
+  return result;
+};
+
+export const authCheck = async () => {
+  console.log("@/composables/firebase-service > authCheck()");
+  return new Promise(function (resolve) {
+    state.loading = true;
+    if (!state.initialized) {
+      getAuth().onAuthStateChanged(async (_user) => {
+        // USER.value = getAuth().currentUser;
+        // console.log('JS USER', getAuth().currentUser);
+        // resolve(currentUser);
+        console.log("_user", _user);
+        if (_user) {
+          state.user = _user;
+        } else {
+          state.user = null;
+        }
+        state.loading = false;
+        state.initialized = true;
+        resolve(true);
+      });
+    }
+  });
+};
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,33 +2,46 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Sample App</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">Sample App</ion-title>
         </ion-toolbar>
       </ion-header>
 
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <div v-if="!user">Not Authenticated</div>
+        <div v-if="user">
+          <p>Authenticated as {{ user.displayName }}</p>
+        </div>
+
+        <div class="ion-margin-top"><ion-button>Login with Google</ion-button></div>
+        <div><ion-button>Login with Apple</ion-button></div>
+        <div><ion-button>Logout</ion-button></div>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { computed } from "vue";
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from "@ionic/vue";
+import { state, googleSignin, appleSignin } from "@/composables/firebase-service";
+
+const user = computed(() => {
+  console.log("state", state);
+  return state?.user ? state.user : null;
+});
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -44,9 +57,9 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
